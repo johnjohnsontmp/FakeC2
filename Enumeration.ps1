63b21045a68fa9f545ebbee44add95a9ef7131c7
johnjohnsontmp <johnjohnsontmp@gmail.com>
2024-08-23 07:08:39 -0700

    Add files via upload

---
+++ Enumeration.ps1
@@ -0,0 +1,12 @@
+function LDAPSearch {
+ param (
+ [string]$LDAPQuery
+ )
+ $PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
+New-Object System.DirectoryServices.DirectoryEntry("LDAP://$PDC") 
+ $DistinguishedName = ([adsi]'').distinguishedName
+ $DirectoryEntry = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$PDC")  
+ $DirectorySearcher = New-Object System.DirectoryServices.DirectorySearcher($DirectoryEntry, $LDAPQuery)
+ return $DirectorySearcher.FindAll()
+}
+
